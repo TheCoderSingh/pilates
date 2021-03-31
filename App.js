095fb7df4d8ff5a28579d37b6476e6d1359f6a12
Jaskaran Singh <jaskaran1523@gmail.com>
2021-03-31 01:10:40 -0700

    Change path

--- App.js
+++ App.js
@@ -117,7 +117,8 @@ export default function App() {
 	return (
 		<NativeRouter>
 			<View style={{ flex: 1 }}>
-				<Route exact path="/" component={Home} />
+				{/* <Route exact path="/" component={Home} /> */}
+				<Route exact path="/" component={SignIn} />
 				<Route exact path="/h/:ps" component={Home} />
 				<Route exact path="/pay" component={Payment} />
 				{/* <Route exact path="/" component={Payment} /> */}
