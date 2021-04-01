e27242ba5d6467457213b8603b376c283790e6cc
Jaskaran Singh <jaskaran1523@gmail.com>
2021-03-31 23:49:54 -0700

    Fix challenge video

--- components/Challenges/Challenge.js
+++ components/Challenges/Challenge.js
@@ -59,55 +59,112 @@ const Challenge = (props) => {
 							</Text>
 							<View style={styles.classes}>
 								{item.classes.map((video, index) => {
-									return (
-										<Link
-											to={"/video/" + video.video_id}
-											style={{ marginBottom: 20 }}
-										>
-											<View>
-												<Text
-													style={{
-														textAlign: "center",
-														marginTop: 15,
-														backgroundColor:
-															"#EFA7A1",
-														height: 30,
-														paddingTop: 8,
-														color: "#FFF",
-														fontWeight: "bold",
-													}}
-												>
-													Day {index + 1}
-												</Text>
+									if (video.video_id) {
+										return (
+											<Link
+												to={"/video/" + video.video_id}
+												style={{ marginBottom: 20 }}
+											>
 												<View>
-													<Image
-														source={{
-															uri:
-																video.class
-																	.image,
-														}}
-														style={{
-															width:
-																deviceWidth -
-																60,
-															height: 200,
-															resizeMode:
-																"contain",
-														}}
-													/>
 													<Text
 														style={{
 															textAlign: "center",
-															fontSize: 18,
-															marginTop: 10,
+															marginTop: 15,
+															backgroundColor:
+																"#EFA7A1",
+															height: 30,
+															paddingTop: 8,
+															color: "#FFF",
+															fontWeight: "bold",
 														}}
 													>
-														{video.title}
+														Day {index + 1}
 													</Text>
+													<View>
+														<Image
+															source={{
+																uri:
+																	video.class
+																		.image,
+															}}
+															style={{
+																width:
+																	deviceWidth -
+																	60,
+																height: 200,
+																resizeMode:
+																	"contain",
+															}}
+														/>
+														<Text
+															style={{
+																textAlign:
+																	"center",
+																fontSize: 18,
+																marginTop: 10,
+															}}
+														>
+															{video.title}
+														</Text>
+													</View>
 												</View>
-											</View>
-										</Link>
-									);
+											</Link>
+										);
+									} else {
+										return (
+											<Link
+												to={
+													"/video/" +
+													video.class.vimeo_id
+												}
+												style={{ marginBottom: 20 }}
+											>
+												<View>
+													<Text
+														style={{
+															textAlign: "center",
+															marginTop: 15,
+															backgroundColor:
+																"#EFA7A1",
+															height: 30,
+															paddingTop: 8,
+															color: "#FFF",
+															fontWeight: "bold",
+														}}
+													>
+														Day {index + 1}
+													</Text>
+													<View>
+														<Image
+															source={{
+																uri:
+																	video.class
+																		.image,
+															}}
+															style={{
+																width:
+																	deviceWidth -
+																	60,
+																height: 200,
+																resizeMode:
+																	"contain",
+															}}
+														/>
+														<Text
+															style={{
+																textAlign:
+																	"center",
+																fontSize: 18,
+																marginTop: 10,
+															}}
+														>
+															{video.title}
+														</Text>
+													</View>
+												</View>
+											</Link>
+										);
+									}
 								})}
 							</View>
 						</View>
